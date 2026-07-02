dde441aada3c66fa86b67c59c95ace1720cfe58b
SAMUEL OWORA CHUKWU <passion4soln@gmail.com>
2026-07-02 14:02:40 +0100

    Week 1 Day 1: Set up JavaScript mentorship project and completed first JavaScript program

--- week-01/day-01/script.js
+++ week-01/day-01/script.js
@@ -1,11 +1,27 @@
-console.log("My name  is Samuel!");
+/*
+=============================================
+Week 1 - day 1
+JavaScript Mentorship Program
+Student : Samuel Owora Chukwu
+Topic : Introduction To JavaScript
+
+
+=============================================
+
+*/
+
+console.log("==========================");
+console.log("    My Introduction       ");
+console.log("==========================");
+
+console.log("My name is Samuel!");
 console.log("I am 38 years old.");
 console.log("I am from Nigeria.");
-console.log("my hobby is solving problems, teaching and coding.");
+console.log("My hobby is solving problems, teaching and coding.");
 console.log("My occupation :  Student");
 console.log("My favourite programming language is JavaScript for now. ");
 
 console.log("My dream is to become a senior software and AI engineer.");
-console.log("My goal for this mentorship it to learn a skill that i can use to solve problems");
-console.log("My favourite quote is Life doesn't give you what you want but what you fight for.");
+console.log("My goal for this mentorship is it to learn a skill that i can use to solve problems");
+console.log("My favourite quote is: 'Life doesn't give you what you want but what you fight for.'");
 
