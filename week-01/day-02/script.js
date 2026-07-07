088788bc31cd4311f296cfe309dc9a1a98f80e2f
SAMUEL OWORA CHUKWU <passion4soln@gmail.com>
2026-07-07 13:51:24 +0100

    Week 1 Day 2: Learned variables, memory, data types, and built a personal profile generator

---
+++ week-01/day-02/script.js
@@ -0,0 +1,53 @@
+let fullName = "Samuel Owora Chukwu";
+const age = 30;
+const state = "Ebonyi";
+const country = "Nigeria";
+const maritalStatus = "Single";
+const religion = "Christianity";
+let favoriteColor = "Royal Blue";
+const favoriteBook = "Zoe by Kenneth E. Hagin";
+const favoriteFood = "Roasted Plantain";
+const favoriteLanguage = "Igbo";
+const programmingLanguage = "JavaScript";
+let currentlyLearning = "Variables";
+let dreamJob = "Software Engineer";
+const yearsOfExperienceLearningProgramming = 2;
+let enjoysProblemSolving = "yes";
+
+
+console.log("=================Personal Profile Generator================");
+
+console.log(`Name                                     :${fullName}`);
+console.log(`Age                                      :${age}`);
+console.log(`State Of Origin                          :${state}`);
+console.log(`Country                                  :${country}`);
+console.log(`Marital Status                           :${maritalStatus}`);
+console.log(`Religion                                 :${religion}`);
+console.log(`Favourite Color                          :${favoriteColor}`);
+console.log(`Favourite Book                           :${favoriteBook}`);
+console.log(`Favourite Food                           :${favoriteFood}`);
+console.log(`Favourite Language                       :${favoriteLanguage}`);
+console.log(`Pogramming Language                      :${programmingLanguage}`);
+console.log(`Curently Learning                        :${currentlyLearning}`);
+console.log(`Dream Job                                :${dreamJob}`);
+console.log(`Years of experience Learning programming :${yearsOfExperienceLearningProgramming}`);
+console.log(`Do you enjoy problem solving             :${enjoysProblemSolving}`);
+
+console.log(`============================================================`);
+console.log(typeof fullName);
+console.log(typeof age);
+console.log(typeof state);
+console.log(typeof country);
+console.log(typeof maritalStatus);
+console.log(typeof religion);
+console.log(typeof favoriteColor); 
+console.log(typeof favoriteBook);
+console.log(typeof favoriteFood);
+console.log(typeof favoriteLanguage);
+console.log(typeof programmingLanguage);
+console.log(typeof currentlyLearning);
+console.log(typeof dreamJob);
+console.log(typeof yearsOfExperienceLearningProgramming);
+console.log(typeof enjoysProblemSolving);
+
+
